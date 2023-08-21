0f4db386a7636a9b144107c3affae7af7da4dd73
lymanhthang <139944186+lymanhthang@users.noreply.github.com>
2023-08-21 15:23:32 +0700

    upload source code

---
+++ JS/js_myProject.js
@@ -0,0 +1,34 @@
+$(document).ready(function() {
+    $(".back2top").hide()
+
+    $(window).scroll(function() {
+        if($(this).scrollTop()>=300)
+            $(".back2top").show(250)
+        else 
+            $(".back2top").hide()
+    })
+})
+function loadNews() {
+    fetch("/JSON/data.json").then(res => res.json()).then(data => {
+        let h ="";
+        for(let p of data)
+            h += `
+            <li class="post"><a href="${p.link}">
+                    <div class="flex flexCol">
+                        <div class="thumb"><img src="/image/${p.img}" alt="img" ></div>
+                        <div class="flex flexCol">
+                            <label>${p.label}</label>
+                            <h4>${p.title}</h4>
+                        </div>
+                    </div>
+                </a></li>
+            `;
+        let e = document.getElementById("allnews");
+        if (e!==null) {
+            e.innerHTML = h;
+        }
+    })
+}
+window.onload = function() {
+    loadNews();
+}
